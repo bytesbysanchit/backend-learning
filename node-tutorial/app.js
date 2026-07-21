286206b481dd38a0dc53d74548889add1952e4cc
Sanchit <sanchitagl88@gmail.com>
2026-07-21 20:09:49 +0530

    learned built-in modules

--- node-tutorial/app.js
+++ node-tutorial/app.js
@@ -1,12 +1,17 @@
-// Modules 
+const http= require('http')
 
-const names= require('./4-names');
-const sayHi= require('./5-utils');
-const data= require('./6-alternative-flavor');
+const server= http.createServer((req, res)=>{
+  if (req.url === '/') {
+    res.end('Welcome to our home page')
+  } else if (req.url === '/about') {
+    res.end('Here is our short history')
+  } else {
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">back home</a>
+    `)
+  }
+})
 
-// console.log(data);
-
-sayHi('Sanchit')
-sayHi(names.john)
-sayHi(names.peter)
-
+server.listen(5000);
