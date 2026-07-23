9d2372db9ba81ae2da0593d36dc33190d918d605
Sanchit <sanchitagl88@gmail.com>
2026-07-23 23:57:13 +0530

    Learn Event loop

--- node-tutorial/app.js
+++ node-tutorial/app.js
@@ -1,6 +1,7 @@
-const _ = require('lodash')
+// const _ = require('lodash')
 
-const items= [1,[2,[3,[4]]]]
-const newItems= _.flattenDeep(items);
-console.log(newItems);
-console.log("Hello People");
+// const items= [1,[2,[3,[4]]]]
+// const newItems= _.flattenDeep(items);
+// console.log(newItems);
+// console.log("Hello People");
+
