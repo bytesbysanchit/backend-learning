e775f873510092aff49492f42d8b02e4d75177df
Sanchit <sanchitagl88@gmail.com>
2026-08-02 02:09:54 +0530

    learned MongoDB

---
+++ 03-MongoDB/02-reading.mongodb.js
@@ -0,0 +1,16 @@
+use('ecommerce');
+
+// db.products.find();
+// db.products.find().pretty()
+// db.products.find({ category: "Electronics" })
+
+// db.products.find({ price: { $gt: 1000 } })     // greater than 1000
+// db.products.find({ price: { $gte: 1000, $lte: 50000 } })
+// db.products.find({ $or: [{ category: "Electronics" }, { stock: { $lt: 50 } }] })
+
+// db.products.find({}, { name: 1, price: 1, _id: 0 })
+// db.products.find().sort({ price:-1 }).skip(0).limit(1)
+
+
+
+
