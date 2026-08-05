7421c202dff0ea0615faec57ca9f8e7d92652cad
Sanchit <sanchitagl88@gmail.com>
2026-08-05 22:51:52 +0530

    learn more features in mongodb

--- 04-Creating-RestAPI/controllers/product.js
+++ 04-Creating-RestAPI/controllers/product.js
@@ -1,9 +1,7 @@
 const Product= require('../models/product');
-const { options } = require('../routes/product');
-
 const getAllProducts= async(req, res)=>{
 
-  const {company, name}= req.query;
+  const {company, name, featured, sort, select}= req.query;
   const queryObject= {};
 
   if(company){
@@ -18,13 +16,32 @@ const getAllProducts= async(req, res)=>{
     queryObject.name= {$regex: name, $options: 'i'};
   }
 
+  let apiData= Product.find(queryObject)
+
+  if(sort){
+    let sortFix= sort.replace(',', ' ')
+    apiData= apiData.sort(sortFix)
+  }
+
+  if(select){
+    let selectFix= select.split(',').join(' ')
+    apiData= apiData.select(selectFix)
+  }
+
+  let page= Number(req.query.page) || 1;
+  let limit= Number(req.query.limit) || 3;
+
+  let skip= (page-1)*limit;
+
+  apiData= apiData.skip(skip).limit(limit);
+
   console.log(queryObject);
 
-  const myData= await Product.find(queryObject)
+  const myData= await apiData
   res.status(200).json({myData})
 }
 const getAllProductsTesting= async(req, res)=>{
-  const myData= await Product.find(req.query)
+  const myData= await Product.find(req.query).select()
   res.status(200).json({myData})
 }
 
