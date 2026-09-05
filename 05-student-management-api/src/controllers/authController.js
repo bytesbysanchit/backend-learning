01164f9fc3dddd4deec77e8674c73215b9fcc1f4
Sanchit <sanchitagl88@gmail.com>
2026-09-05 21:09:20 +0530

    feat: add user registration and login authentication

--- 05-student-management-api/src/controllers/authController.js
+++ 05-student-management-api/src/controllers/authController.js
@@ -13,14 +13,45 @@ const register= async(req, res, next)=>{
       password: hashedPassword
     })
 
+    const userData = user.toObject();
+    delete userData.password;
+    
+
     res.status(201).json({
       message: 'User registered successfully',
-      user
+      user: userData
     });
 
   } catch(error){
     next(error);
   }
 }
+
+
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user === null) {
+      return res.status(401).json({
+        message: 'Invalid email or password'
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: 'Invalid email or password'
+      });
+    }
+
+  } catch (error) {
+    next(error);
+  }
+};
+
 
 module.exports= {register}
